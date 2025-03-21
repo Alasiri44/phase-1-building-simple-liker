cf371d16837ad8f7f9a00fb3255ae03a93778a62
alasiri <austinalasiri44@gmail.com>
2025-03-21 12:10:43 +0300

    Allowed liking

--- main.js
+++ main.js
@@ -4,9 +4,37 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const errorModal = document.getElementById('modal');
+errorModal.classList.add('hidden');
 
+const hearts = document.getElementsByClassName('like-glyph');
 
+for(let heart of hearts){
+  heart.addEventListener('click', function(event){
+      console.log(heart);
+      mimicServerCall()
+      .then(event => {
+        if(!heart.classList.contains('activated-heart')){
+          heart.textContent = FULL_HEART;
+          heart.classList.add('activated-heart');
 
+        }else{
+          heart.textContent = EMPTY_HEART;
+          heart.classList.remove('activated-heart');
+        }
+      })
+      .catch(error => {
+        errorModal.classList.remove('hidden');
+             
+        errorModal.innerHTML = `
+          <p>${error.message}</p>
+        `;
+        setTimeout( function() {
+          errorModal.classList.add('hidden')
+        },3000)
+      })
+    })
+  }
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
